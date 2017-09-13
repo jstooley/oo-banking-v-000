0f9e186de90581d4459640730b84ba3b1b00c318
Justin Tooley <jstooley823@gmail.com>
2017-09-13 23:50:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,13 @@
 class Transfer
   def initialize(sender, receiver, amount)
     @sender = sender
-    @receiver = receiver 
+    @receiver = receiver
     @amount = amount
+  end
+  def sender
+    @sender
+  end
+  def receiver
+    @receiver
   end
 end
